c46aaa9ced623c20f39ca10313e2172034bec6ca
Jaloliddin Rustamov <mr.romaboy@gmail.com>
2019-10-24 22:34:50 +0800

    Initial commit

---
+++ app/src/main/java/com/android/example/AboutMe/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.android.example.AboutMe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    //task 7 & 8 left
+}
